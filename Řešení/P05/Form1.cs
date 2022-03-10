71c1ce1da82b7f5fdb8f8005f03b1ae958a63aa5
Jan Polách <polachj.05@spst.cz>
2022-03-10 11:34:18 +0100

    P05 almost done

--- Řešení/P05/Form1.cs
+++ Řešení/P05/Form1.cs
@@ -16,5 +16,76 @@ namespace P05
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int n = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int[] pole = new int[n];
+            int[] pole2;
+            int[] pole3;
+            bool je_b = false;
+            for (int k = 0; k < n; k++)
+            {
+                pole[k] = rnd.Next(1, 20);
+                listBox1.Items.Add(pole[k]);
+                if (pole[k] == b)
+                    je_b = true;
+                
+            }
+            Array.Sort(pole);
+            listBox1.Items.Clear();
+            for (int k = 0; k < n; k++)
+            {
+                listBox1.Items.Add(pole[k]);
+            }
+            if (je_b)
+            {
+                int indmax = Array.IndexOf(pole, b);
+                int indmaxl = Array.LastIndexOf(pole, b);
+                pole2 = pole.Take(indmax + 1).ToArray();
+                pole3 = pole.Skip(indmaxl + 1).ToArray();
+                for (int k = 0; k < pole2.Length; k++)
+                {
+                    listBox2.Items.Add(pole2[k]);
+                }
+                for (int k = 0; k < pole3.Length; k++)
+                {
+                    listBox3.Items.Add(pole3[k]);
+                }
+            }
+            else
+            {
+                int y = 0;
+                int z = 0;
+                
+                for (int x = 0; x < pole.Length; x++)
+                {
+
+                    if (pole[x] <= b)
+                    {
+                        pole2[y] = pole[x];
+                        y++;
+                    }
+                    else
+                    {
+                        pole3[z] = pole[x];
+                        z++;
+                    }
+                }
+
+
+
+                foreach (int p in pole2)
+                {
+                    listBox2.Items.Add(p);
+                }
+                foreach (int p in pole3)
+                {
+                    listBox3.Items.Add(p);
+                }
+            }
+        }
     }
 }

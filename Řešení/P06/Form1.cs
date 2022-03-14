7c70571f5a03245280fdc4bb0f8108905fdce6f3
Jan Polách <polachj.05@spst.cz>
2022-03-14 09:34:26 +0100

    P06 + P07 Done

---
+++ Řešení/P06/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        double[] pole;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int n = Convert.ToInt32(textBox1.Text);
+            double[] pole = new double[n];
+            for(int v = 0; v < n; v++)
+            {
+                pole[v] = rnd.NextDouble() * 2000 - 1000;
+                listBox1.Items.Add(pole[v]);
+            }
+            double avg = pole.Average();
+            double max = -1001;
+            foreach(double cislo in pole)
+            {
+                if (cislo <= avg)
+                    if (cislo > max)
+                        max = cislo;
+            }
+            MessageBox.Show("Největší číslo menší nebo rovno průměru" + avg + " je " + max, "Max menší než průměr", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Řešení/P07/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] polecelk = new int[listBox1.Items.Count];
+            int cislo = Convert.ToInt32(listBox1.Items[0]);
+            int c=0;
+            for (int v = 0; cislo != 0; v++)
+            {   
+                polecelk[v] = cislo;
+                cislo = Convert.ToInt32(listBox1.Items[v+1]);
+                c = v;
+            }
+            MessageBox.Show("První prvek dělen posledním prvkem je " + polecelk[0] / polecelk[c],"První/Poslední", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            for(int b = 0; b < c; b++)
+            {
+                if (polecelk[b] < polecelk[c])
+                    listBox2.Items.Add(polecelk[b] + " Je menší než " + polecelk[c] + " a jeho index je " + b);
+            }
+        }
+    }
+}
